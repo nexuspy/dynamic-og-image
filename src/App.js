3bd0c2d27fb2bbd0f43695653d5d0579835be94b
nexuspy <59784880+nexuspy@users.noreply.github.com>
2024-08-04 16:14:20 +0530

    dpp image genration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import PostPage from './components/PostPage';
+
+function App() {
+  return (
+    <div className="App">
+      <PostPage />
+    </div>
+  );
+}
+
+export default App;
